51e4e01a9cdd5ae49002bb5ed13f1ba557fbe672
Drake Bott <bott.da@gmail.com>
2018-11-28 10:40:46 -0700

    Create migrations to add timestamps and add foreign key

---
+++ db/migrations/20181128093950_add-timestamp.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('palettes', function(table) {
+      table.timestamps(true, true);
+    }),
+    knex.schema.table('projects', function(table) {
+      table.timestamps(true, true);
+    }),
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.al([
+    knex.schema.table('palettes', function(table) {
+      table.dropColumn('created_at');
+      table.dropColumn('updated_at');
+    }),
+    knex.schema.table('projects', function(table) {
+      table.dropColumn('created_at');
+      table.dropColumn('updated_at');
+    })
+  ]);
+};
