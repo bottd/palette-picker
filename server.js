84a284893c8714de9dd1316649329b2269e7a349
Drake Bott <bott.da@gmail.com>
2018-11-27 17:24:43 -0700

    Create endpoints to pull from database

--- server.js
+++ server.js
@@ -8,8 +8,38 @@ const Palette = require('./palette');
 app.use(express.static('public'));
 
 app.get('/colors', (req, res) => {
-  const colors = new Palette(5)
+  const colors = new Palette(5);
   res.status(200).json(colors);
+});
+
+app.get('/projects', async (req, res) => {
+  try {
+    const projects = await database('projects').select();
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(500).json({error});
+  }
+});
+
+app.get('/projects/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const palettes = await database('palettes')
+      .where('project_id', id)
+      .select();
+    res.status(200).json(palettes);
+  } catch (error) {
+    res.status(500).json({error});
+  }
+});
+
+app.get('/palettes', async (req, res) => {
+  try {
+    const palettes = await database('palettes').select();
+    res.status(200).json(palettes);
+  } catch (error) {
+    res.status(500).json({error});
+  }
 });
 
 app.listen(3000, () => {
