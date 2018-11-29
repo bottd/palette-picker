cf2dd3f67d7f681f05cade05598b20a8683886cd
Drake Bott <bott.da@gmail.com>
2018-11-29 13:51:34 -0700

    Add hoverable dropdown

--- public/script.js
+++ public/script.js
@@ -1,14 +1,20 @@
 const cardContainer = document.querySelector('.card-container');
 const shuffleButton = document.querySelector('.shuffle');
+const projectHeader = document.querySelector('.project-name');
 
 cardContainer.addEventListener('click', toggleLock);
 shuffleButton.addEventListener('click', setColors);
+projectHeader.addEventListener('keyup', blurOnEnter);
+
+const project = {
+  name: '',
+  id: null
+}
 
 function generateColors(num) {
   let colors = [];
   const randomHex = () => {
-    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
-    return hex[Math.floor(Math.random() * 16)];
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']; return hex[Math.floor(Math.random() * 16)];
   };
   const randomColor = () => {
     let color = '#';
@@ -19,7 +25,6 @@ function generateColors(num) {
   };
 
   for (let i = 0; i < num; i++) {
-    console.log(randomColor());
     colors.push(randomColor());
   }
   return colors;
@@ -30,12 +35,12 @@ function setColors() {
   const colors = generateColors(unlockedCards.length);
   unlockedCards.forEach((card, i) => {
     card.style.borderColor = colors[i];
+    card.innerText = colors[i];
     card.parentElement.style.backgroundColor = colors[i];
   });
 };
 
 function toggleLock(e) {
-  console.log(e.target.style.backgroundColor);
   if (e.target.classList.contains('fa-lock-open')) {
     e.target.classList.remove('fa-lock-open');
     e.target.classList.add('fa-lock');
@@ -44,6 +49,15 @@ function toggleLock(e) {
     e.target.classList.remove('fa-lock');
     e.target.classList.add('fa-lock-open');
   }
+}
+
+function blurOnEnter(e) {
+  project.name = e.target.value;
+  const key = e.keyCode || e.charCode;
+  if (key === 13) {
+    projectHeader.blur();
+  }
+  console.log(project.name);
 }
 
 setColors();
