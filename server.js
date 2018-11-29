b69e7a946d460690d49b426a0c60c1119129d094
Drake Bott <bott.da@gmail.com>
2018-11-28 20:26:38 -0700

    Re-order express endpoints, update paths to be under /api/v1

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const database = require('knex')(configuration);
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.get('/projects', async (req, res) => {
+app.get('/api/v1/projects', async (req, res) => {
   try {
     const projects = await database('projects').select();
     res.status(200).json(projects);
@@ -17,32 +17,37 @@ app.get('/projects', async (req, res) => {
   }
 });
 
-app.get('/projects/:id', async (req, res) => {
+app.get('/api/v1/projects/:id', async (req, res) => {
   try {
     const id = req.params.id;
     const project = await database('projects')
       .where('id', id)
       .select();
-    const palettes = await database('palettes')
-      .where('project_id', id)
-      .select();
-    res.status(200).json({project, palettes});
+    res.status(200).json(project);
   } catch (error) {
     res.status(500).json({error});
   }
 });
 
-app.get('/palettes', async (req, res) => {
+app.post('/api/v1/projects', async (req, res) => {
+  const project = req.body;
+  const id = await database('projects').insert(project, 'id');
+  res.status(200).json({id});
+});
+
+app.get('/api/v1/projects/:project_id/palettes', async (req, res) => {
   try {
-    const palettes = await database('palettes').select();
+    const project_id = req.params.project_id
+    const palettes = await database('palettes').where('project_id', project_id).select();
     res.status(200).json(palettes);
   } catch (error) {
     res.status(500).json({error});
   }
 });
 
-app.get('/palettes/:id', async (req, res) => {
+app.get('/api/v1/projects/:project_id/palettes/:id', async (req, res) => {
   try {
+    const project_id = req.params.project_id;
     const id = req.params.id;
     const palettes = await database('palettes')
       .where('id', id)
@@ -53,13 +58,8 @@ app.get('/palettes/:id', async (req, res) => {
   }
 });
 
-app.post('/projects', async (req, res) => {
-  const project = req.body;
-  const id = await database('projects').insert(project, 'id');
-  res.status(200).json({id});
-});
 
-app.post('/palettes', async (req, res) => {
+app.post('/api/v1/palettes', async (req, res) => {
   const palette = req.body;
   const id = await database('palettes').insert(palette, 'id');
   res.status(200).json({id});
