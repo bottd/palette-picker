fbb75d0b6467db65f15c4836d82bbbad4f54bae2
Drake Bott <bott.da@gmail.com>
2018-11-29 14:34:05 -0700

    Load project palettes to page

--- public/script.js
+++ public/script.js
@@ -5,16 +5,18 @@ const projectHeader = document.querySelector('.project-name');
 cardContainer.addEventListener('click', toggleLock);
 shuffleButton.addEventListener('click', setColors);
 projectHeader.addEventListener('keyup', blurOnEnter);
+projectHeader.addEventListener('blur', updateProjectName);
 
 const project = {
   name: '',
-  id: null
-}
+  id: null,
+};
 
 function generateColors(num) {
   let colors = [];
   const randomHex = () => {
-    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']; return hex[Math.floor(Math.random() * 16)];
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+    return hex[Math.floor(Math.random() * 16)];
   };
   const randomColor = () => {
     let color = '#';
@@ -28,7 +30,7 @@ function generateColors(num) {
     colors.push(randomColor());
   }
   return colors;
-};
+}
 
 function setColors() {
   const unlockedCards = document.querySelectorAll('.fa-lock-open');
@@ -38,24 +40,58 @@ function setColors() {
     card.innerText = colors[i];
     card.parentElement.style.backgroundColor = colors[i];
   });
-};
+}
 
 async function loadProjects() {
   const dropdown = document.querySelector('.dropdown');
   const res = await fetch('api/v1/projects');
   const projects = await res.json();
-  const projectLi = projects.map(project => (
-    `<li class="alternate-title" id='${project.id}'>${project.name}</li>`
-  ))
+  const projectLi = projects.map(
+    project =>
+      `<li class="alternate-title" id='${project.id}'>${project.name}</li>`,
+  );
+  dropdown.innerHTML = ('');
   dropdown.innerHTML = projectLi.join('');
+  const projectNames = document.querySelectorAll('.alternate-title');
+  projectNames.forEach(name => name.addEventListener('click', setProject));
+}
+
+async function setProject(e) {
+  project.name = e.target.innerText;
+  project.id = e.target.id;
+  projectHeader.value = project.name;
+  const res = await fetch(`api/v1/projects/${project.id}/palettes`);
+  const palettes = await res.json();
+  const colorCards = document.querySelectorAll('.color');
+  if (palettes.length === 0) {
+    setColors();
+    return;
+  }
+  colorCards.forEach((card, i) => {
+    card.style.borderColor = palettes[0][`hex${i + 1}`];
+    card.innerText = palettes[0][`hex${i + 1}`].toUpperCase();
+    card.parentElement.style.backgroundColor = palettes[0][`hex${i + 1}`];
+  });
+}
+
+async function updateProjectName(e) {
+  project.name = e.target.value;
+  await fetch(`/api/v1/projects/${project.id}`, {
+    method: 'PUT',
+    body: JSON.stringify(project),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  loadProjects();
+
 }
 
 function toggleLock(e) {
   if (e.target.classList.contains('fa-lock-open')) {
     e.target.classList.remove('fa-lock-open');
     e.target.classList.add('fa-lock');
-  }
-  else if (e.target.classList.contains('fa-lock')) {
+  } else if (e.target.classList.contains('fa-lock')) {
     e.target.classList.remove('fa-lock');
     e.target.classList.add('fa-lock-open');
   }
@@ -67,7 +103,6 @@ function blurOnEnter(e) {
   if (key === 13) {
     projectHeader.blur();
   }
-  console.log(project.name);
 }
 
 setColors();
